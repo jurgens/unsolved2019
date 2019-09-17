49b05db6cafc4fb4988fc2573f52591715cd7c67
Yura Omelchuk <jurgen@kodolabs.com>
2019-09-17 16:58:34 +0200

    manually set CORS headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  after_action :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
 end
